7c1e50343961910d0cc022d6692193a8888a4118
Aditya-devop <141511902+Aditya-devop@users.noreply.github.com>
2024-04-11 23:54:27 +0530

    Add files via upload

---
+++ Menu_script.js
@@ -0,0 +1,34 @@
+let headerLi = document.querySelectorAll(".navbar ul li");
+headerLi.forEach((list) => {
+    list.addEventListener("click", () => {
+        headerLi.forEach((lst) => {
+            lst.classList.remove("active_anchor");
+        });
+        list.classList.add("active_anchor");
+    });
+});
+
+let lists = document.querySelectorAll(".menu_option li");
+let menuContainer = document.querySelector(".menu_container");
+let menus = document.querySelectorAll(".all");
+
+lists.forEach((list) => {
+    list.addEventListener("click", () => {
+        lists.forEach((lst) => {
+            lst.classList.remove("active");
+        });
+        list.classList.add("active");
+        //menu change code
+        let dataFilter = list.getAttribute("data-filter").slice(1);
+        menus.forEach((menu) => {
+            menu.classList.remove("show");
+            menu.classList.add("hide");
+            setTimeout(() => {
+                if (menu.classList.contains(dataFilter)) {
+                    menu.classList.remove('hide');
+                    menu.classList.add('show');
+                }
+            }, 100)
+        });
+    });
+});
